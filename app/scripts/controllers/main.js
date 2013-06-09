5929f29e55fc159e092c9cb5b6fb1f7a35c0962a
guifroes <guilherme.froes@gmail.com>
2013-06-09 19:37:43 -0300

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('newPokerclockApp')
+  .controller('MainCtrl', function ($scope, $timeout) {
+    var startTimer = function(){
+      return $timeout($scope.onTimeout, 1000);
+    }
+
+    $scope.gameName = 'My awesome poker game';
+    $scope.gameStarted = true;
+
+    var counter = 1125;
+    $scope.onTimeout = function(){
+      counter--;
+      $scope.time = new Date(0, 0, 0, 0, 0, counter, 0);
+      mytimeout = startTimer();
+    }
+
+    var mytimeout = startTimer();
+
+    var stopTimer = function() {
+      $timeout.cancel(mytimeout);
+    }
+
+    $scope.stop = function(){
+      if($scope.gameStarted){
+        stopTimer();
+        $scope.gameStarted = false;
+      }
+      else {
+        $scope.gameStarted = true;
+        mytimeout = startTimer();
+      }
+    }
+});
