a586df0286397b583c99c7e73b5878b57a8e65a0
guifroes <guilherme.froes@gmail.com>
2014-03-19 00:07:05 -0300

    Adding factories

---
+++ test/spec/clock/levelModel.js
@@ -0,0 +1,55 @@
+'use strict'
+
+describe('LevelsModel', function () {
+
+  var levelsSet = {};
+  beforeEach(function () {
+    module('newPokerclock.factories');
+
+    inject(function(_levels_) {
+      levelsSet = _levels_;
+    });
+  });
+
+  it('should have 10 levels by default', function () {
+    expect(levelsSet.levels.length).toBe(10);
+  });
+
+  it('each level should have number, value and time', function () {
+    levelsSet.levels.forEach(function (level) {
+      expect(level.number).toBeDefined();
+      expect(level.value).toBeDefined();
+      expect(level.time).toBeDefined();
+    })
+  });
+
+  it('each level should have bigger number, value and same time as previous', function () {
+    for(var i = 0; i < levelsSet.levels.length - 1; i++) {
+      var previousNumber = levelsSet.levels[i].number;
+      var number = levelsSet.levels[i + 1].number;
+
+      expect(previousNumber < number).toBe(true);
+
+      var previousValue = levelsSet.levels[i].value;
+      var value = levelsSet.levels[i + 1].value;
+
+      expect(previousValue < value).toBe(true);
+    }
+  });
+
+  it('should add a level', function () {
+    var previousNumberOfLevels = levelsSet.levels.length;
+
+    levelsSet.addLevel();
+
+    expect(levelsSet.levels.length).toBe(previousNumberOfLevels + 1);
+  });
+
+  it('should remove a level', function () {
+    var previousNumberOfLevels = levelsSet.levels.length;
+
+    levelsSet.removeLevel(5);
+
+    expect(levelsSet.levels.length).toBe(previousNumberOfLevels - 1);
+  });
+});
