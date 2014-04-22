4a315ea260a6c82e6f794ca130ad3a0b6225c347
guifroes <guilherme.froes@gmail.com>
2014-04-21 23:12:58 -0300

    Deleting bower components

--- app/js/timer/timerDisplayDirective.js
+++ app/js/timer/timerDisplayDirective.js
@@ -1,6 +1,5 @@
 angular.module('newPokerclock.directives').directive('timerDisplay', function () {
   return function (scope, elem, attrs) {
     var date = new Date(0, 0, 0, 0, 0, scope.count, 0);
-    moment().format('MMMM Do YYYY, h:mm:ss a');
   };
 });
